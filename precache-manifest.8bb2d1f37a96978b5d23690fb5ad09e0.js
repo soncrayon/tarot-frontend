2502815aa085611a646911ec52d203a07f099b94
soncrayon <arnold.curtis@gmail.com>
2020-11-20 21:35:09 -0500

    Updates

---
+++ precache-manifest.8bb2d1f37a96978b5d23690fb5ad09e0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76d7000a3bab2e8dea56b9b6125bcc76",
+    "url": "/ori-tarot/index.html"
+  },
+  {
+    "revision": "6e74a7f91900ed94d0a6",
+    "url": "/ori-tarot/static/css/main.b5bb95fe.chunk.css"
+  },
+  {
+    "revision": "7e6d15baf5bbfceac3d9",
+    "url": "/ori-tarot/static/js/2.0c030318.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ori-tarot/static/js/2.0c030318.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e74a7f91900ed94d0a6",
+    "url": "/ori-tarot/static/js/main.603d8f07.chunk.js"
+  },
+  {
+    "revision": "f4c4d5d3185c621e2e15",
+    "url": "/ori-tarot/static/js/runtime-main.e2e49dea.js"
+  },
+  {
+    "revision": "9f14152e68114363ccb4f34cbd39a0a3",
+    "url": "/ori-tarot/static/media/card_back.9f14152e.jpeg"
+  },
+  {
+    "revision": "2624d50e8f848e17c3caa29e3f43de2d",
+    "url": "/ori-tarot/static/media/theme_background.2624d50e.jpg"
+  }
+]);

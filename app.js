c620f71a3858bebb9c2c4577a4007c941e2e42e7
soncrayon <arnold.curtis@gmail.com>
2020-12-30 00:30:20 -0500

    add'l edits for deployment

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express")
 const app = express()
 
 // use the express-static middleware
-app.use(express.static("public"))
+app.use(express.static("build"))
 
 // define the first route
 app.get("/", function (req, res) {

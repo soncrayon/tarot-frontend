f623e54c171431b284f71e8dfa769fd0a3b0bc27
soncrayon <arnold.curtis@gmail.com>
2020-12-30 00:24:00 -0500

    add'l edits for deployment--add express and procfile

---
+++ app.js
@@ -0,0 +1,15 @@
+// create an express app
+const express = require("express")
+const app = express()
+
+// use the express-static middleware
+app.use(express.static("public"))
+
+// define the first route
+app.get("/", function (req, res) {
+    res.sendFile('build/index.html', {root: __dirname });
+})
+
+// start the server listening for requests
+app.listen(process.env.PORT || 3000, 
+	() => console.log("Server is running..."));
